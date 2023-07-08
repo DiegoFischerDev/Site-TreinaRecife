a8e589164561fdbc58a35f2c7671b380b004f9da
DiegoFischerDev <diegofischer.dev@gmail.com>
2023-07-08 15:57:26 +0100

    @fix adjustment consultoria responsivity

--- src/componentes/CardConsultoria.js
+++ src/componentes/CardConsultoria.js
@@ -6,7 +6,7 @@ const CardConsultoria = (props) => {
 
   return (
     <section>
-      <div className="card" style={{ width: 280, padding: 10, marginInline: 10}}>
+      <div className="card" style={{ width: "90vw", maxWidth: "400px", padding: 10, marginInline: 10, marginBottom: "30px"}}>
         <div style={{border: '1px solid #E4E2E2', borderRadius: 10}}><img src={props.imagem} className="card-img-top" alt="Imagem do Curso" style={{height: 150, borderWidth: 2, borderRadius: 10}}/></div>
         <div className="card-body">
           <h5 className="card-title">{props.titulo}</h5>
--- src/pages/Consultoria/index.js
+++ src/pages/Consultoria/index.js
@@ -5,12 +5,12 @@ const Consultoria = () => {
     <div style={{display: "flex", alignItems: "center", flexDirection: "column"}}>
       <h2 style={{marginTop: 50, textAlign: "center"}}>Consultoria Para Empresas</h2>
 
-      <div style={{ border: '1px solid #E4E2E2', borderRadius: 10, marginTop: 50, padding: 20, maxWidth: 1000, marginBottom: 30}}>
+      <div style={{ border: '1px solid #E4E2E2', borderRadius: 10, marginTop: 50, padding: 20, width:"90vw", maxWidth: 1200, marginBottom: 30}}>
       <p style={{textAlign: "justify"}}>A Treina Recife desenvolveu um Programa que combina recrutamento com capacitação. Nós selecionamos os candidatos a desenvolvedores e submetemos a testes. Os classificados participam de treinamento, custeado pela empresa parceira, com possibilidade de contratação dependendo da performance do aluno. O treinamento é baseado na tecnologia que a empresa parceira utiliza. Ao longo do processo, são analisados os hard e soft skills. 
       </p>
       </div>
-      <div style={{flexDirection: "row", display: "flex", width: "100%", justifyContent: "center", marginTop: 30}}>
-        <CardConsultoria style={{gap: 10}} titulo="Avançar" descricao="Treinamentos de novas tecnologias para colaboradores da área de tecnologia." imagem="https://mobiliza.com.br/wp-content/uploads/2020/08/achievement-3468104.jpg" />
+      <div style={{flexDirection: "row", display: "flex", width: "100%", maxWidth: "1500px", justifyContent: "center", marginTop: 30, flexWrap: "wrap"}}>
+        <CardConsultoria titulo="Avançar" descricao="Treinamentos de novas tecnologias para colaboradores da área de tecnologia." imagem="https://mobiliza.com.br/wp-content/uploads/2020/08/achievement-3468104.jpg" />
 
         <CardConsultoria titulo="Migrar" descricao="Treinamentos partindo do zero, para funcionários de outras áreas da empresa." imagem="https://res.cloudinary.com/di9oiqvom/image/upload/v1680396987/imagemMigrar_jdmzjj.png" />
 
